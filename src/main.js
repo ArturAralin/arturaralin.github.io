23436c910c9f5e56747d00519d104e2e41018c89
Artur A <artur.aralin97@gmail.com>
2019-01-21 21:17:07 +0300

    update: fix text, add new reference

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
 require('normalize.css');
 require('./styles/main.scss');
+
+// Dirty hack for development
+// I promise to solve this
+if (process.env.NODE_ENV === 'development') {
+  document.querySelector('link').remove();
+}
